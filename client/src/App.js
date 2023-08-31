846b136701b3d9080675d30f478215e38704d5b7
BekCodingAddict <experte001@gmail.com>
2023-09-01 00:01:53 +0900

    Installing Front-End and Back-End npm dependencies!

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import {Button} from 'antd';
+import 'antd/dist/reset.css';
+
+function App() {
+  return (
+    <div className="App m-5">
+      <h1>Hello World!</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
