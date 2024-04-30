ae79c9f0df543d0f4ccddfa923b0c59a6e55d5ff
BekCodingAddict <experte001@gmail.com>
2024-04-30 11:49:11 +0900

    Updated!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 9000;
 app.use(express.json());
 
 const usersRoute = require("./routes/usersRoute");
